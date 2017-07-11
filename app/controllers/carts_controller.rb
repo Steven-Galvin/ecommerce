1ca1eb34f1b54fea6c01df715b879309860ef6af
James Higgins and Monique St. Laurent <james@>
2017-07-11 09:58:28 -0700

    Add stripe form to cart view and add gloabl amount variable

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,5 +2,26 @@ class CartsController < ApplicationController
 
   def show
     @order_items = current_order.order_items
+    @amount = (current_order.total_price)
+  end
+
+  def create
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
+
+    rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+    # end
   end
 end
